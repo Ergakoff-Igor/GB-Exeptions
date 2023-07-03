2967d09b46ce3fa617962009af72fad5ce0a794e
Igor <i.ergakoff@mail.ru>
2023-07-03 18:20:45 +0300

    GB. Extentions. Homework 2. Task 4

--- Homework_2/Program.java
+++ Homework_2/Program.java
@@ -7,13 +7,14 @@ public class Program {
         Task1 task1 = new Task1();
         Task2 task2 = new Task2();
         Task3 task3 = new Task3();
+        Task4 task4 = new Task4();
 
         // Task 1:
-        // System.out.printf("Вы ввели: %.2f",task1.inputFloatNum());
+        System.out.printf("Вы ввели: %.2f",task1.inputFloatNum());
 
         // Task 2:
-        // int[] arrayTask2 = { 1, 5, 3, 3, 6, 15, 4, 5, 5, 1 };
-        // task2.resultCodeTask2(arrayTask2, 0, 8);
+        int[] arrayTask2 = { 1, 5, 3, 3, 6, 15, 4, 5, 5, 1 };
+        task2.resultCodeTask2(arrayTask2, 0, 8);
 
         // Task 3:
         int a = 90;
@@ -25,6 +26,7 @@ public class Program {
         int[] abc = { 1, 2 };
         task3.changeArray(abc, 3, 9);
 
-
+        // Task 4:
+        System.out.println("Введена строка: " + task4.inputValue());
     }
 }
--- Homework_2/StringInputException.java
+++ Homework_2/StringInputException.java
@@ -1,4 +1,9 @@
 
-public class StringInputException {
+public class StringInputException extends Exception{
+    public StringInputException() {
+    }
 
+    public StringInputException(String message) {
+        super(message);
+    }
 }
